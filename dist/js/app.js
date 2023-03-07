ef5ab1ba385b2c2405966a119ca6d34c9f95e4a1
Felipe Lima <felipe.lima@FLEX.LOCAL>
2023-03-07 17:57:17 -0300

    teste

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,3 +5,16 @@ form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adiciona();
 });
+
+// neste modelo conseguimos colocar dinâmicamente uma propriedade em um objeto
+
+const objeto = {
+    nome: "Felipe",
+    sobreNome: "Oliveira"
+}
+
+console.log(objeto)
+
+objeto.naturalidade = "BR"
+
+console.log(objeto)
